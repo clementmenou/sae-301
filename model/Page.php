83297fe2530c467986fad75a76c1826eafe4e2bb
Clément Menou <clementmenou0@gmail.com>
2023-12-16 04:30:02 +0100

    bonne progression sur Page.php

--- model/Page.php
+++ model/Page.php
@@ -1,1 +1,62 @@
 <?php
+
+class Page
+{
+    public $title;
+    public $styles;
+    public $scripts;
+    public $header;
+    public $body;
+    public $footer;
+
+    public function __construct($title, $body, $styles, $scripts, $header, $footer)
+    {
+        $this->title = $title;
+        $this->styles = $styles;
+        $this->header = $header;
+        $this->body = $body;
+        $this->footer = $footer;
+        $this->scripts = $scripts;
+    }
+
+    public function render()
+    {
+        $this->renderHead();
+        $this->renderHeader();
+        $this->renderBody();
+        $this->renderFooter();
+        $this->renderFoot();
+    }
+
+    private function renderHead()
+    {
+        $title = $this->title;
+        $styles = $this->styles;
+        include_once './view/head.php';
+    }
+
+    private function renderHeader()
+    {
+        if ($this->header) {
+            include_once './view/header.php';
+        }
+    }
+
+    private function renderBody()
+    {
+        include_once './view/' . $this->body;
+    }
+
+    private function renderFooter()
+    {
+        if ($this->footer) {
+            include_once './view/footer.php';
+        }
+    }
+
+    private function renderFoot()
+    {
+        $scripts = $this->scripts;
+        include_once './view/foot.php';
+    }
+}
