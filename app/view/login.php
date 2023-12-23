7f5c40c1ca5dae851fab2f2c70384e95c8e70ef6
Clément Menou <clementmenou0@gmail.com>
2023-12-23 16:03:31 +0100

    Add class="input-error" to input error msg

--- app/view/login.php
+++ app/view/login.php
@@ -3,14 +3,14 @@
         <label for="input-email">Email</label>
         <input type="email" name="email" id="input-email" class="input-large" value="<?= htmlspecialchars($datas['email']) ?>" required>
         <?php if ($datas['login_status'] === 'EMAIL_INCORRECT') : ?>
-            <div>Email incorrect</div>
+            <div class="input-error">Email incorrect</div>
         <?php endif; ?>
     </div>
     <div class="input-area">
         <label for="input-password">Password</label>
         <input type="password" name="password" id="input-password" class="input-large" value="<?= htmlspecialchars($datas['password']) ?>" required>
         <?php if ($datas['login_status'] === 'PASSWORD_INCORRECT') : ?>
-            <div>Mot de passe incorrect</div>
+            <div class="input-error">Mot de passe incorrect</div>
         <?php endif; ?>
     </div>
     <input type="submit" value="Connexion">
