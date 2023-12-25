636e1c64d976b48932a2eea0135ddbfb57d007db
Clément Menou <clementmenou0@gmail.com>
2023-12-25 05:53:56 +0100

    Session helper upgraded

--- app/helpers/SessionHelper.php
+++ app/helpers/SessionHelper.php
@@ -4,20 +4,72 @@ namespace App\Helpers;
 
 class SessionHelper
 {
-    public static function setSessionValue($key, $value)
+    public static function setSessionValue($key, $value, $subKey = null)
     {
         self::startSession();
 
         if (self::isSessionActive()) {
-            $_SESSION[$key] = $value;
+            if ($subKey !== null) {
+                // Set sub variable
+                if (!isset($_SESSION[$key])) {
+                    $_SESSION[$key] = [];
+                }
+
+                $_SESSION[$key][$subKey] = $value;
+            } else {
+                // Set main variable
+                $_SESSION[$key] = $value;
+            }
         }
     }
 
-    public static function getSessionValue($key)
+    public static function getSessionValue($key, $subKey = null)
     {
         self::startSession();
 
-        return self::isSessionActive() ? ($_SESSION[$key] ?? null) : null;
+        if (self::isSessionActive()) {
+            if ($subKey !== null) {
+                return $_SESSION[$key][$subKey] ?? null;
+            } else {
+                return $_SESSION[$key] ?? null;
+            }
+        }
+
+        return null;
+    }
+
+    public static function unsetSessionValue($key, $subKey = null)
+    {
+        self::startSession();
+
+        if (self::isSessionActive()) {
+            if ($subKey !== null) {
+                // Suppr sub variable
+                if (isset($_SESSION[$key][$subKey])) {
+                    unset($_SESSION[$key][$subKey]);
+                }
+            } else {
+                // Suppr main variable
+                if (isset($_SESSION[$key])) {
+                    unset($_SESSION[$key]);
+                }
+            }
+        }
+    }
+
+    public static function sessionValueExists($key, $subKey = null)
+    {
+        self::startSession();
+
+        if (self::isSessionActive()) {
+            if ($subKey !== null) {
+                return isset($_SESSION[$key][$subKey]);
+            } else {
+                return isset($_SESSION[$key]);
+            }
+        }
+
+        return false;
     }
 
     private static function isSessionActive()
