edcd86df5cf0891b11a6d76524dea2bd06aaf315
Clément Menou <clementmenou0@gmail.com>
2023-12-25 05:26:45 +0100

    Ajout des URL dans helpers

--- app/helpers/RedirectHelper.php
+++ app/helpers/RedirectHelper.php
@@ -4,6 +4,11 @@ namespace App\Helpers;
 
 class RedirectHelper
 {
+    // URL
+    public const HOME_URL = '/';
+    public const LOGIN_URL = '/connectez_vous';
+    public const SIGNUP_URL = '/inscrivez_vous';
+
     public static function redirectTo($location)
     {
         header("Location: $location");
