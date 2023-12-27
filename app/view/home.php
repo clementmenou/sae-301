1e5e32b05fa20f78674d809618610c421fe0839f
Clément Menou <clementmenou0@gmail.com>
2023-12-27 03:47:36 +0100

    Test of css

--- app/view/home.php
+++ app/view/home.php
@@ -1,9 +1,14 @@
 <form action="/" method="POST">
-    <button type="submit" name="fragrance" value="hesperides">Hespéridés</button>
-    <button type="submit" name="fragrance" value="fleuris">Fleuris</button>
-    <button type="submit" name="fragrance" value="boises">Boisés</button>
-    <button type="submit" name="fragrance" value="fougeres">Fougères</button>
-    <button type="submit" name="fragrance" value="chypres">Chyprés</button>
-    <button type="submit" name="fragrance" value="orientaux">Orientaux</button>
-    <button type="submit" name="fragrance" value="aromatiques">Aromatiques</button>
+    <button class="" type="submit" name="fragrance" value="hesperides">Hespéridés</button>
+    <button class="" type="submit" name="fragrance" value="fleuris">Fleuris</button>
+    <button class="" type="submit" name="fragrance" value="boises">Boisés</button>
+    <button class="" type="submit" name="fragrance" value="fougeres">Fougères</button>
+    <button class="" type="submit" name="fragrance" value="chypres">Chyprés</button>
+    <button class="" type="submit" name="fragrance" value="orientaux">Orientaux</button>
+    <button class="" type="submit" name="fragrance" value="aromatiques">Aromatiques</button>
 </form>
+
+<h1 class="title">Titre</h1>
+<p class="text">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus deleniti nulla blanditiis quas, at magnam architecto neque explicabo maxime ab nihil recusandae quo, facere nisi aut sed deserunt, excepturi iusto?
+</p>
