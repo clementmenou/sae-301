7cf687f2ccb7ac17e449b2a079473c4284fe3302
Clément Menou <clementmenou0@gmail.com>
2023-12-27 03:47:54 +0100

    Connection button

--- app/view/header.php
+++ app/view/header.php
@@ -1,9 +1,12 @@
-<header id="header">
-    <a href="/" id="header-logo">Logo</a>
-    <div id="header-link-zone">
+<header class="header">
+    <a href="/" class="header-logo">Logo</a>
+    <div class="header-link-zone">
         <a href="/regardez_nos_produits">Parfums</a>
         <a href="/connectez_vous">Login</a>
         <a href="/inscrivez_vous">Sign Up</a>
         <a href="/ezrpofjez">Error 404</a>
     </div>
+    <form action="/deconnexion" method="post">
+        <input type="submit" value="Deconnection">
+    </form>
 </header>
