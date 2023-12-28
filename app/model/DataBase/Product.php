87839ab18a33d550479d4ec7ebb15edfef95b891
Clément Menou <clementmenou0@gmail.com>
2023-12-28 20:20:30 +0100

    Add getAllProduct in model

--- app/model/DataBase/Product.php
+++ app/model/DataBase/Product.php
@@ -6,4 +6,10 @@ use App\Model\DataBase\DataBase;
 
 class Product extends DataBase
 {
+    public function getAllUsers()
+    {
+        $sql = "SELECT * FROM $this->dbName.products WHERE status = active";
+        $stmt = $this->getConnection()->query($sql);
+        return $stmt->fetch();
+    }
 }
