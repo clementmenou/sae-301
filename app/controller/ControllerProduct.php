1ff24c4c3e77ab0efc312ed6ade92338d70ead91
Clément Menou <clementmenou0@gmail.com>
2023-12-28 20:20:22 +0100

    Add ControllerProduct

---
+++ app/controller/ControllerProduct.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+// Model
+use App\Model\DataBase\Product;
+
+// Helpers
+use App\Helpers\{
+    FormHelper as Form,
+    RedirectHelper as Redirect,
+    SessionHelper as Session
+};
+
+class ControllerProduct
+{
+    private $product;
+
+    public function __construct()
+    {
+        $this->product = new Product();
+    }
+
+    public function productList()
+    {
+        $this->product->getAllUsers();
+    }
+}
