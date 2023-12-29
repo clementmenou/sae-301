7718887fb09aaa242eed3d9634730dae90d16ed3
Clément Menou <clementmenou0@gmail.com>
2023-12-29 01:17:08 +0100

    Model fix getAllProducts

--- app/model/DataBase/Product.php
+++ app/model/DataBase/Product.php
@@ -6,7 +6,7 @@ use App\Model\DataBase\DataBase;
 
 class Product extends DataBase
 {
-    public function getAllUsers()
+    public function getAllProducts()
     {
         $sql = "SELECT * FROM $this->dbName.products WHERE status = 'active'";
         $stmt = $this->getConnection()->query($sql);
