c142b67374e1bd2e8ccab3aa209a47167d7541fd
Clément Menou <clementmenou0@gmail.com>
2024-01-01 19:01:23 +0100

    ControllerOrder create order function

--- app/controller/ControllerOrder.php
+++ app/controller/ControllerOrder.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+// Model
+use App\Model\DataBase\Order;
+
 // Helpers
 use App\Helpers\{
     FormHelper as Form,
@@ -11,7 +14,21 @@ use App\Helpers\{
 
 class ControllerOrder
 {
+    private $order;
+
+    public function __construct()
+    {
+        $this->order = new Order();
+    }
+
     public function addToOrder()
     {
+        $user_id = Session::getValue('user_id');
+        $address_id = Session::getValue('address_id');
+
+        if ($user_id) {
+            $order_id = $this->order->insert($user_id, $address_id);
+            Session::setValue('order_id', null, $order_id);
+        }
     }
 }
