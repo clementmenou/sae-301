7dd23fbae7ad9ecaeb03b526dde3f41f319a50cc
Clément Menou <clementmenou0@gmail.com>
2024-01-01 19:00:46 +0100

    Fix Order model

--- app/model/DataBase/Order.php
+++ app/model/DataBase/Order.php
@@ -6,25 +6,28 @@ use App\Model\DataBase\DataBase;
 
 class Order extends DataBase
 {
-    public function insert($datas)
+    public function insert($id, $address)
     {
         $sql = "INSERT INTO $this->dbName.orders (
-                user_id
+                user_id,
+                address_id
             ) 
             VALUES (
-                :user_id
+                :user_id,
+                :address_id
             )";
         $params = [
-            'user_id' => $datas['userId']
+            'user_id' => $id,
+            'address_id' => $address
         ];
         try {
             // Transaction for multiples request
             $this->getConnection()->beginTransaction();
             $this->getConnection()->prepare($sql)->execute($params);
-            $userId = $this->getConnection()->lastInsertId();
+            $orderId = $this->getConnection()->lastInsertId();
             $this->getConnection()->commit();
 
-            return $userId;
+            return $orderId;
         } catch (\PDOException $e) {
             // Cancel if error in transaction
             $this->getConnection()->rollBack();
