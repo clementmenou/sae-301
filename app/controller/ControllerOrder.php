5c303e6422b510f080480de424bd74e3b13dc17b
Clément Menou <clementmenou0@gmail.com>
2024-01-04 00:51:25 +0100

    ControllerOrder amélioration d'ajout au panier

--- app/controller/ControllerOrder.php
+++ app/controller/ControllerOrder.php
@@ -25,10 +25,24 @@ class ControllerOrder
     {
         $user_id = Session::getValue('user_id');
         $address_id = Session::getValue('address_id');
+        $order_id = Session::getValue('order_id');
+        $button_pressed = Form::validate('add_to_order', ['required' => true]);
+        if (Form::validate('item_id', [
+            'required' => true,
+            'is_number' => true,
+            'max_lenght' => 10
+        ])) {
+            $item_id = Form::getValue('item_id');
+        }
 
-        if ($user_id) {
+        if ($button_pressed && $user_id && !$order_id) {
             $order_id = $this->order->insert($user_id, $address_id);
             Session::setValue('order_id', null, $order_id);
+        }
+
+        if ($button_pressed && $order_id && $item_id) {
+            // fonction ajout d'un item
+            // sauvegarde de l'item dans la liste des items du panier sous Session::'list_item_id'
         }
     }
 }
