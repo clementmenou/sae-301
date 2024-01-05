b4b09798e2c3b1c8edafa428a237bfe4bad3b75d
Clément Menou <clementmenou0@gmail.com>
2024-01-05 19:27:58 +0100

    Test on product_list view

--- app/view/product_list.php
+++ app/view/product_list.php
@@ -1,9 +1,12 @@
 <p>Produits</p>
 <?php
 foreach ($datas['all_products'] as $product) : ?>
-    <div>
+    <form action="/regardez_nos_produits" method="POST">
+        <input type="hidden" name="product_id" value="<?= $product['product_id'] ?>">
         <div><?= $product['name'] ?></div>
         <div><?= $product['description'] ?></div>
         <div><?= $product['price'] ?> €</div>
-    </div>
+        <input type="number" name="quantity" value="1">
+        <input type="submit" name="add_to_order" value="Ajouter au panier">
+    </form>
 <?php endforeach ?>
