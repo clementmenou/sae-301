780f3fd9e0d102fc43bf6cb336362dbb01a99b8b
Clément Menou <clementmenou0@gmail.com>
2024-01-07 16:49:42 +0100

    Correct product_list view

--- app/view/product_list.php
+++ app/view/product_list.php
@@ -3,10 +3,10 @@
 foreach ($datas['all_products'] as $product) : ?>
     <form action="/regardez_nos_produits" method="POST">
         <input type="hidden" name="product_id" value="<?= $product['product_id'] ?>">
-        <div><?= $product['name'] ?></div>
-        <div><?= $product['description'] ?></div>
-        <div><?= $product['price'] ?> €</div>
-        <input type="number" name="quantity" value="1">
-        <input type="submit" name="add_to_order" value="Ajouter au panier">
+        <button name="redirect_product_info" type="submit">
+            <div><?= $product['name'] ?></div>
+            <div><?= $product['description'] ?></div>
+            <div><?= $product['price'] ?> €</div>
+        </button>
     </form>
 <?php endforeach ?>
