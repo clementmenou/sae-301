ebcfce2866c5681039e5781ed72ba197f5d8102a
Clément Menou <clementmenou0@gmail.com>
2024-01-07 16:51:15 +0100

    Create methods of product_info in ControlerProduct

--- app/controller/ControllerProduct.php
+++ app/controller/ControllerProduct.php
@@ -27,4 +27,19 @@ class ControllerProduct
         $data['all_products'] = $this->product->getProductsByCategory(Session::getValue('fragrance'));
         return $data;
     }
+
+    public function redirectToProductInfo()
+    {
+        if (Form::validate('product_id', ['required' => true, 'is_number' => true, 'max_length' => 10])) {
+            Session::setValue('product_info_id', null, Form::getValue('product_id'));
+            Redirect::redirectTo(Redirect::PRODUCT_INFO_URL);
+        }
+    }
+
+    public function productInfo()
+    {
+        $data = [];
+        $data = $this->product->getById(Session::getValue('product_info_id'));
+        return $data;
+    }
 }
