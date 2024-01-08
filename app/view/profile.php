bab9bc42622af8bd24888a6990551e3f673d9826
Clément Menou <clementmenou0@gmail.com>
2024-01-08 21:13:47 +0100

    Draft profile view

---
+++ app/view/profile.php
@@ -0,0 +1,7 @@
+<div>
+    <div>Nom : <?= $datas['last_name'] ?></div>
+    <div>Prénom : <?= $datas['first_name'] ?></div>
+    <div>Pseudo : <?= $datas['username'] ?></div>
+    <div>Email : <?= $datas['email'] ?></div>
+    <div>Mot de passe : <?= $datas['password'] ?></div>
+</div>
