36bc7201a60bcb95c89ca75ed642d3231c83c0ba
Clément Menou <clementmenou0@gmail.com>
2024-01-08 21:15:25 +0100

    Add url of profile get and post

--- app/helpers/RedirectHelper.php
+++ app/helpers/RedirectHelper.php
@@ -8,6 +8,7 @@ class RedirectHelper
     public const HOME_URL = '/';
     public const LOGIN_URL = '/connectez_vous';
     public const SIGNUP_URL = '/inscrivez_vous';
+    public const PROFILE_URL = '/votre_profil';
     public const PRODUCT_LIST_URL = '/regardez_nos_produits';
     public const PRODUCT_INFO_URL = '/informations_sur_le_produit';
     public const LOGOUT_URL = '/deconnexion';
