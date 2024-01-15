4a800aa841204d257dc27930f250da099660657d
Clément Menou <clementmenou0@gmail.com>
2024-01-16 00:24:29 +0100

    ajout insert Category dans Category model

--- app/model/DataBase/Category.php
+++ app/model/DataBase/Category.php
@@ -6,4 +6,25 @@ use App\Model\DataBase\DataBase;
 
 class Category extends DataBase
 {
+    public function insertProductCategory($product_id, $category_id)
+    {
+        $sql = "INSERT INTO 
+            $this->dbName.productcategories(
+                category_id,
+                product_id
+            )
+            VALUES (
+                :category_id,
+                :product_id
+            )";
+        $params = [
+            'category_id' => $category_id,
+            'product_id' => $product_id
+        ];
+        try {
+            $this->getConnection()->prepare($sql)->execute($params);
+        } catch (\PDOException $e) {
+            throw new \Exception('User insertion failed: ' . $e->getMessage());
+        }
+    }
 }
