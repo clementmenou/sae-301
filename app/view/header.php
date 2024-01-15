9877117d5218f2465dd76a779d266747c5ac7805
Clément Menou <clementmenou0@gmail.com>
2024-01-16 00:25:58 +0100

    lien manage dans header

--- app/view/header.php
+++ app/view/header.php
@@ -6,6 +6,7 @@
         <a class="header-link" href="/connectez_vous">Login</a>
         <a class="header-link" href="/inscrivez_vous">Sign Up</a>
         <a class="header-link" href="/votre_profil">Profile</a>
+        <a class="header-link" href="/manage">Manage</a>
         <form action="/deconnexion" method="post">
             <input type="submit" value="Deconnection">
         </form>
