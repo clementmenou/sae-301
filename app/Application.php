4272a9235d38ca9d1cc85b9f347c5d8f7ace5cc0
Clément Menou <clementmenou0@gmail.com>
2024-01-16 00:26:06 +0100

    lien order dans Application

--- app/Application.php
+++ app/Application.php
@@ -33,6 +33,7 @@ class Application
         $this->router->get(Redirect::PRODUCT_LIST_URL, 'ControllerPage->product_list');
         $this->router->get(Redirect::PRODUCT_INFO_URL, 'ControllerPage->product_info');
         $this->router->get(Redirect::MANAGE_URL, 'ControllerPage->manage');
+        $this->router->get(Redirect::ORDER_URL, 'ControllerPage->order');
         $this->router->get(Redirect::LOGIN_URL, 'ControllerPage->login');
         $this->router->get(Redirect::SIGNUP_URL, 'ControllerPage->signup');
         $this->router->get(Redirect::PROFILE_URL, 'ControllerPage->profile');
@@ -44,7 +45,8 @@ class Application
         $this->router->post(Redirect::HOME_URL, 'ControllerPage->home');
         $this->router->post(Redirect::PRODUCT_LIST_URL, 'ControllerPage->product_list');
         $this->router->post(Redirect::PRODUCT_INFO_URL, 'ControllerPage->product_info');
-        $this->router->get(Redirect::MANAGE_URL, 'ControllerPage->manage');
+        $this->router->post(Redirect::MANAGE_URL, 'ControllerPage->manage');
+        $this->router->post(Redirect::ORDER_URL, 'ControllerPage->order');
         $this->router->post(Redirect::LOGIN_URL, 'ControllerPage->login');
         $this->router->post(Redirect::SIGNUP_URL, 'ControllerPage->signup');
         $this->router->post(Redirect::PROFILE_URL, 'ControllerPage->profile');
