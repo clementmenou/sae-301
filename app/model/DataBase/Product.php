0e9e3771ff9666bc9cd4e2ce55dc220d3818c42b
Clément Menou <clementmenou0@gmail.com>
2024-01-16 00:25:07 +0100

    ajout masse fonctionnalité Product model

--- app/model/DataBase/Product.php
+++ app/model/DataBase/Product.php
@@ -28,6 +28,21 @@ class Product extends DataBase
         return $stmt->fetchColumn();
     }
 
+    public function getIdByName($name)
+    {
+        $sql = "SELECT product_id FROM $this->dbName.products WHERE name = :name AND status = 'active'";
+        $params = [
+            'name' => $name
+        ];
+        try {
+            $stmt = $this->getConnection()->prepare($sql);
+            $stmt->execute($params);
+            return $stmt->fetchColumn();
+        } catch (\PDOException $e) {
+            throw new \Exception('User insertion failed: ' . $e->getMessage());
+        }
+    }
+
     public function getProductsByCategory($category)
     {
         $sql = "SELECT *
@@ -56,5 +71,84 @@ class Product extends DataBase
         WHERE p.status = 'active' AND pc.status = 'active'";
         $stmt = $this->getConnection()->query($sql);
         return $stmt->fetchAll();
+    }
+
+    public function getAllNames()
+    {
+        $sql = "SELECT name FROM $this->dbName.products WHERE status = 'active'";
+        $stmt = $this->getConnection()->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    public function updateQuantity($datas)
+    {
+        $sql = "UPDATE 
+                $this->dbName.products
+            SET
+                stock_quantity = :stock_quantity
+            WHERE 
+                product_id = :product_id";
+        $params = [
+            'stock_quantity' => $datas['stock_quantity'],
+            'product_id' => $datas['product_id']
+        ];
+        try {
+            $this->getConnection()->prepare($sql)->execute($params);
+        } catch (\PDOException $e) {
+            throw new \Exception('User insertion failed: ' . $e->getMessage());
+        }
+    }
+
+    public function isProductByName($name)
+    {
+        $sql = "SELECT EXISTS (SELECT 1 FROM $this->dbName.products WHERE name = :name AND status = 'active') AS name_exists";
+        $stmt = $this->getConnection()->prepare($sql);
+        $params = [
+            'name' => $name
+        ];
+        $stmt->execute($params);
+        return $stmt->fetchColumn();
+    }
+
+    public function insert($datas)
+    {
+        $sql = "INSERT 
+            INTO 
+                $this->dbName.products (name, description, price, stock_quantity, image)
+            VALUES
+                (:name, :description, :price, :stock_quantity, :image)";
+        $params = [
+            'name' => $datas['name'],
+            'description' => $datas['description'],
+            'price' => $datas['price'],
+            'stock_quantity' => $datas['stock_quantity'],
+            'image' => $datas['image']
+        ];
+        try {
+            // Transaction for multiples request
+            $this->getConnection()->beginTransaction();
+            $this->getConnection()->prepare($sql)->execute($params);
+            $productId = $this->getConnection()->lastInsertId();
+            $this->getConnection()->commit();
+
+            return $productId;
+        } catch (\PDOException $e) {
+            // Cancel if error in transaction
+            $this->getConnection()->rollBack();
+            throw new \Exception('User insertion failed: ' . $e->getMessage());
+        }
+    }
+
+    public function delete($id)
+    {
+        $sql = "UPDATE $this->dbName.products SET status = 'inactive' WHERE product_id = :product_id";
+        $params = [
+            'product_id' => $id
+        ];
+        try {
+            $this->getConnection()->prepare($sql)->execute($params);
+        } catch (\PDOException $e) {
+            throw new \Exception('User insertion failed: ' . $e->getMessage());
+        }
     }
 }
