cd36d994841c5e1540bd3ffadd132485e26bb9b6
Clément Menou <clementmenou0@gmail.com>
2024-01-16 03:37:35 +0100

    address view

---
+++ app/view/address.php
@@ -0,0 +1,31 @@
+<form action="/address" method="POST">
+    <div>Street</div>
+    <input type="text" name="street" value="<?= $datas['insert']['street'] ?>">
+    <div>City</div>
+    <input type="text" name="city" value="<?= $datas['insert']['city'] ?>">
+    <div>Zip_code</div>
+    <input type="text" name="zip_code" value="<?= $datas['insert']['zip_code'] ?>">
+    <div>Region</div>
+    <input type="text" name="region" value="<?= $datas['insert']['region'] ?>">
+    <div>Country</div>
+    <input type="text" name="country" value="<?= $datas['insert']['country'] ?>">
+    <input type="submit" name="insert" value="Ajouter">
+</form>
+
+<?php foreach ($datas['user_addresses'] as $address) : ?>
+    <form action="/address" method="POST">
+        <input type="text" name="street" value="<?= $address['address_id'] ?>">
+        <div>Street</div>
+        <input type="text" name="street" value="<?= $address['street'] ?>">
+        <div>City</div>
+        <input type="text" name="city" value="<?= $address['city'] ?>">
+        <div>Zip_code</div>
+        <input type="text" name="zip_code" value="<?= $address['zip_code'] ?>">
+        <div>Region</div>
+        <input type="text" name="region" value="<?= $address['region'] ?>">
+        <div>Country</div>
+        <input type="text" name="country" value="<?= $address['country'] ?>">
+        <input type="submit" name="update" value="Modifier">
+        <input type="submit" name="delete" value="Supprimer">
+    </form>
+<?php endforeach; ?>
