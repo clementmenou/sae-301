928f732ba201f2c30144db3e420d2d5b3ccd926f
Clément Menou <clementmenou0@gmail.com>
2024-01-18 13:12:53 +0100

    add required to payment view

--- app/view/payment.php
+++ app/view/payment.php
@@ -3,29 +3,29 @@
         <div class="form-title">Paiement</div>
 
         <div class="input-label-area">
-            <input type="text" id="input-card-number" maxlength="19" class="input-generic" placeholder="" name="card_number">
+            <input type="text" id="input-card-number" maxlength="19" class="input-generic" placeholder="" name="card_number" required>
             <label for="input-card-number" class="label-generic">Numéro de carte</label>
         </div>
         <div class="form-sub-container">
             <div class="form-sub-container-expiration">
                 <div class="input-label-area-extra-small">
-                    <input type="text" id="input-expiration-month" minlength="2" maxlength="2" class="input-generic" placeholder="" name="expiration_month">
+                    <input type="text" id="input-expiration-month" minlength="2" maxlength="2" class="input-generic" placeholder="" name="expiration_month" required>
                     <label for="input-expiration-month" class="label-generic">XX</label>
                 </div>
                 <div class="input-expiration-spliter">/</div>
                 <div class="input-label-area-extra-small">
-                    <input type="text" id="input-expiration-year" minlength="2" maxlength="2" class="input-generic" placeholder="" name="expiration_year">
+                    <input type="text" id="input-expiration-year" minlength="2" maxlength="2" class="input-generic" placeholder="" name="expiration_year" required>
                     <label for="input-expiration-year" class="label-generic">XX</label>
                 </div>
             </div>
 
             <div class="input-label-area-small">
-                <input type="text" id="input-security-code" class="input-generic" maxlength="3" placeholder="" name="security_code">
+                <input type="text" id="input-security-code" class="input-generic" maxlength="3" placeholder="" name="security_code" required>
                 <label for="input-security-code" class="label-generic">Code</label>
             </div>
         </div>
         <div class="input-label-area">
-            <input type="text" id="input-name" class="input-generic" placeholder="" name="name">
+            <input type="text" id="input-name" class="input-generic" placeholder="" name="name" required>
             <label for="input-name" class="label-generic">Nom du titulaire</label>
         </div>
 
