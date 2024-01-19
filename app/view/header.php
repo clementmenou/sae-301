af8039d21880ac5ac84a2d671080fecb68e2f1ae
Clément Menou <clementmenou0@gmail.com>
2024-01-19 05:04:17 +0100

    home refonte

--- app/view/header.php
+++ app/view/header.php
@@ -1,15 +1,17 @@
 <header class="header">
     <a href="/" class="header-logo"></a>
     <div class="header-link-zone">
-        <a class="header-link" href="/regardez_nos_produits">Parfums</a>
-        <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']) { ?>
+        <a class="header-link" href="/regardez_nos_produits">Boutique</a>
+        <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']) : ?>
             <a class="header-link" href="/manage">Manage</a>
-        <?php }
-        if (!isset($_SESSION['user_id'])) : ?>
-            <a class="header-link" href="/connectez_vous">Login</a>
-        <?php else : ?>
-            <a class="header-link" href="/votre_profil">Profile</a>
         <?php endif; ?>
-        <a class="header-link" href="/votre_panier">Panier</a>
+        <a class="header-link" href="<?php if (!isset($_SESSION['user_id'])) : ?>/connectez_vous<?php else : ?>/votre_profil<?php endif; ?>">
+            <img src="Public/Images/icon-account.png" alt="" class="header-link-image-account">
+            <div>Compte</div>
+        </a>
+        <a class="header-link" href="/votre_panier">
+            <img src="Public/Images/icon-cart.png" alt="" class="header-link-image-cart">
+            <div>Panier</div>
+        </a>
     </div>
 </header>
