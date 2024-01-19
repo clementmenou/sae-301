41956b2b7dc1095e2cb4460dc868fd5da5fb7864
Clément Menou <clementmenou0@gmail.com>
2024-01-19 07:08:24 +0100

    fix + add js to ProductInfo

--- app/model/Pages/ProductInfo.php
+++ app/model/Pages/ProductInfo.php
@@ -11,8 +11,8 @@ class ProductInfo extends Page
         parent::__construct(
             'Products',
             'product_info.php',
-            ['header.css', 'form.css', 'footer.css'],
-            [],
+            ['header.css', 'form.css', 'product_info', 'footer.css'],
+            ['product_info.js'],
             true,
             true
         );
